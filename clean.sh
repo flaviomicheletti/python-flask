cfdd4f07dba28cb03e8e40b8c84a30055c1f3da6
Flávio Micheletti <flaviomicheletti@hotmail.com>
2023-01-22 13:07:22 -0300

    last

---
+++ clean.sh
@@ -0,0 +1,5 @@
+echo "Remove __pycache__ files"
+sudo find . -type d -name __pycache__ -exec rm -r {} \+
+sudo find . -type d -name .pytest_cache -exec rm -r {} \+
+sudo find . -type d -name htmlcov -exec rm -r {} \+
+sudo find . -type d -name .venv -exec rm -r {} \+        
