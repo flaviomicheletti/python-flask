48d65fa80e11b05adce70910f8701b407f0a3b75
Flávio Micheletti <flaviomicheletti@hotmail.com>
2023-01-22 19:26:14 -0300

    cleaning the house

--- python-pdf-imagem/laudo_009.py
+++
@@ -1,8 +0,0 @@
-from laudo_008 import PDF
-
-pdf = PDF()
-pdf.doc_config['title'] = "Laudo Express"
-pdf.laudo['tipo']       = "Laudo Express"
-pdf.laudo['nome']       = "Lucas Andrade"
-pdf.arquivo             = "pdfs/laudo-009.pdf"
-pdf.gerar()
