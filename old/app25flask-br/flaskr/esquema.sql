9725b367f7fbc1eb2752c7dcc3b50a2e3ef86a11
Flávio Micheletti <flaviomicheletti@hotmail.com>
2023-07-06 20:35:38 -0300

    files deleted

--- old/app25flask-br/flaskr/esquema.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists entradas;
-create table entradas (
-  id integer primary key autoincrement,
-  titulo string not null,
-  texto string not null
-);
--- old/app28languages/readme.md
+++
@@ -1,5 +0,0 @@
-
-# na pasta raiz
-coverage run -m unittest discover
-coverage report -m
-coverage html
